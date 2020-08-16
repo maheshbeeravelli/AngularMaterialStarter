8949b363b0ed21f4ec6d357c6890488456ba28a7
Mahesh Beeravelli <Mahesh.Beeravelli@mphasis.com>
2020-08-16 15:57:09 +0530

    MatButton Module Added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
- import {MatIconModule} from '@angular/material/icon'
+ import {MatIconModule} from '@angular/material/icon';
+ import {MatButtonModule} from '@angular/material/button';
 
-const SHARED_MODULES = [MatToolbarModule, MatIconModule];
+const SHARED_MODULES = [MatToolbarModule, MatIconModule, MatButtonModule];
 
 @NgModule({
   declarations: [],
